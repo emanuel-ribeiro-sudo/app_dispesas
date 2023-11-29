7420679e3602ed108230e152d047ea8f9c9994a4
Emanuel Ribeiro <78225903+emanuel-ribeiro-sudo@users.noreply.github.com>
2023-11-29 11:31:00 -0100

    de 16.1 para 16.2

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF234583);
 const letra = Color(0xFF000000);
 const remColor = Color(0xFFFF0000);
-const defaultPadding = 16.1;
+const defaultPadding = 16.2;
 
 const Heroheight = 130.0;
 const Herowidth = 170.0;
